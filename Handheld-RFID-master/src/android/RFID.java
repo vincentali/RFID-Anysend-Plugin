ebef00d8417337c9b59b2d86de957f0905c5c24e
VincentAliChirinpour <VincentAliChirinpour@LAPTOP-9OKA1D9S.fritz.box>
2022-05-11 14:16:44 +0200

    - update

--- Handheld-RFID-master/src/android/RFID.java
+++ Handheld-RFID-master/src/android/RFID.java
@@ -43,57 +43,6 @@ import anysendRFID.IntentFilter;
 import android.content.Intent;
 
 public class RFID extends CordovaPlugin {
-	SearchTagActivity sta = null;
-
-	/**
-	 * Constructor.
-	 */
-	public RFID() {
-	}
-
-	/**
-	 * Sets the context of the Command. This can then be used to do things like get
-	 * file paths associated with the Activity.
-	 *
-	 * @param cordova The context of the main Activity.
-	 * @param webView The CordovaWebView Cordova is running in.
-	 */
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		super.initialize(cordova, webView);
-	}
-
-	/**
-	 * Executes the request and returns PluginResult.
-	 *
-	 * @param action          The action to execute.
-	 * @param args            JSONArry of arguments for the plugin.
-	 * @param callbackContext The callback id used when calling back into
-	 *                        JavaScript.
-	 * @return True if the action was valid, false if not.
-	 */
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if ("readRFID".equals(action)) {
-			this.read(callbackContext);
-		} else if ("cancelRFID".equals(action)) {
-			this.cancel(callbackContext);
-		} else {
-			return false;
-		}
-		return true;
-	}
-
-	public void read(CallbackContext cbCtx) {
-		this.sta = new SearchTagActivity(cbCtx);
-	}
-
-	public void cancel(CallbackContext cbCtx) {
-		// is the cordova plugin implementatio singleton?
-		if (this.sta != null) {
-			// this.sta.mSendingThread.interrupt();
-			this.sta.onDestroy();
-			this.sta = null;
-		}
-	}
 
 	public void enableRfid() {
 	    Intent intent = new Intent("com.handheldgroup.anysend.SET_STATE");
